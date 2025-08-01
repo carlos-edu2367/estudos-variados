a3ce6b743d5087ec5c8ee4e745a15583091d5d55
carlos-edu2367 <contato.carlosedu2367@gmail.com>
2025-08-01 14:42:11 -0300

    Erros em APIS, codigos 400 e 401

--- apis/fastapi/basics/routes/auth_routes.py
+++ apis/fastapi/basics/routes/auth_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from dependecies import pegar_sessao
 from models import Usuario
 from main import bcrypt_context
@@ -21,9 +21,9 @@ async def login(email: str, senha: str, session = Depends(pegar_sessao)):
         if senha_hash == resposta.senha:
             return {"id": resposta.id}
         else:
-            return {"erro": "Senha incorreta"}
+            return HTTPException(status_code=401, detail="Senha incorreta")
     else:
-        return {"mensagem": "email não encontrado"}
+        return HTTPException(status_code=401, detail="E-mail não encontrado")
 
 # Destrinchando esta end point:
 # Primeiro como decorator (@ acima da função) nós setamos que é uma rota referente ao
@@ -35,15 +35,22 @@ async def login(email: str, senha: str, session = Depends(pegar_sessao)):
 async def cadastro( email:str, senha:str, nome:str, session = Depends(pegar_sessao) ):
     """
     Esta rota espera receber as informações do usuário para realizar o cadastro
+
+    Possiveis erros: 
+    400 - E-mail já existe no banco de dados
     """
     usuario = session.query(Usuario).filter(Usuario.email == email).first()
     if usuario:
         # ja existe um usuario cadastrado com esse e-mail
-        return {"mensagem": "E-mail já cadastrado"}
+        return HTTPException(status_code=400, detail="E-mail já cadastrado")
+    
+        # utilizamos o HTTPException para enviar um status de erro para api
+        # ou seja, caso algo de errado enviamos um status com codigo e detalhes do erro
+        # Erros geralmente vão ser 400 ou 401
     else:
         senha_criptografada = bcrypt_context.hash(senha)
         novoUsuario = Usuario(nome, email, senha_criptografada) # deve ser na ordem do init
         session.add(novoUsuario)
         session.commit()
-        return {"mensagem": "Cadastro realizado"}
+        return {"mensagem": f"Cadastro realizado {email}"}
 
